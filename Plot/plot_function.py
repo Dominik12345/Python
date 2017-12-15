4a10d0c99e52d445aea9dea8166cb78bead02774
dominik12345 <dominik.kahl2@gmail.com>
2017-12-15 17:22:11 +0100

    versch

--- Plot/plot_function.py
+++ Plot/plot_function.py
@@ -4,13 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axisartist import Subplot
 
+hfont = {'fontname':'Helvetica'}
+
 
 # simple plot function
 def plot2d(data, variables ,filename):
-    
+
     # default vales
     logscale_x = False
     logscale_y = False
+    logable = True
     
     grid_bool = False
     
@@ -19,8 +22,13 @@ def plot2d(data, variables ,filename):
     axisbottom_bool = True
     axisleft_bool = True
     
+    legend_outside = True
+    
+    colors = {}
+    for i in range(0,len(variables)):
+        colors[variables[i]] = ['w','b','g','r','c','m','y','k'][i]
     # intern parameters
-    plot_range = 1./10  # means of two data series differ by this factor, 
+    plot_range = 1./8  # means of two data series differ by this factor, 
                         # the scalng is defined ill
     
     # prepare data --->
@@ -40,6 +48,19 @@ def plot2d(data, variables ,filename):
     if (not isinstance(data, list)): 
         print('first argument must be of type list')
         return None
+    for j in data[0]:
+        if (not isinstance(j,float) and not isinstance(j,int) ):
+            print('data must be passed as a list of int or float')
+            return None
+    for i in data[1:]:
+        for j in i:
+            if (not isinstance(j,float) and not isinstance(j,int) ):
+                print('data must be passed as list of int or float')
+                return None
+            elif j <= 0 and logable:
+                print('logscale impossible')
+                logable = False # to avoid using logscale
+                
 
     
     #get number of rows and colums of data
@@ -66,16 +87,21 @@ def plot2d(data, variables ,filename):
     for i in variables:
         [bounds['lower',i] , bounds['upper',i] ] = [min(df[i]) , max(df[i])]
     
-    # check whether magnitudes are approximately equal
+    # check whether magnitudes are approximately equal and use logscale if not
     means = [0] * (ncols-1)
-    
-    for i in range(1,ncols):
-        means[i-1] = np.mean( df[ variables[i] ] )    
-        
-    if min(means)/max(means) < plot_range:
-        print('ill scaling - use logscale')
+    for i in range(1,ncols): # compute means of the columns
+        means[i-1] = np.mean( df[ variables[i] ] )
+    if min(means)/max(means) < plot_range and logable:  # if variables have different magnitudes set 
+        print('ill scaling - use logscale')             # logscale
         logscale_y = True
+     
+    for i in range(1,ncols): # if one variable changes magnitude drastically set logscale
+        temp = means[i-1]/(max(df[variables[i]])-min(df[variables[i]]))
         
+        if temp < plot_range and logable and not logscale_y:
+            print('very ill scaling - use logscale')
+            logscale_y = True 
+    
     # <--- analyse data
     
 
@@ -85,12 +111,17 @@ def plot2d(data, variables ,filename):
     fig.add_subplot(ax)
     
     for i in variables[1:]:
-        ax.scatter( df[variables[0]] , df[i] , label = '$'+i+'$' )
+        ax.scatter( df[variables[0]] , df[i] , label = '$'+i+'$',
+                   color = colors[i])
+    # font
     
-    # legend
-    ax.legend()
-    plt.xlabel('$'+variables[0]+'$')
-    
+    # labels and legend
+    ax.legend(loc = 'best')
+    plt.xlabel(variables[0], **hfont)
+    if legend_outside:
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     # logscale
     if logscale_y:
         plt.yscale('log')
@@ -98,18 +129,20 @@ def plot2d(data, variables ,filename):
         plt.xscale('log')
     # grid
     plt.grid(grid_bool)
+
     # visible axis
     ax.axis["right"].set_visible(axisright_bool)
     ax.axis["top"].set_visible(axistop_bool)
     ax.axis["bottom"].set_visible(axisbottom_bool)
     ax.axis["left"].set_visible(axisleft_bool)
     
-    
     # <--- plot
     
     # save plot --->
-    directory = 'C:/Users/kahl/Documents/Images/' + filename + '.pdf'
-    fig.savefig(directory)
+    pdfdirectory = 'C:/Users/kahl/Documents/Images/' + filename + '.pdf'
+    pgfdirectory = 'C:/Users/kahl/Documents/Images/' + filename + '.pgf'
+    fig.savefig(pdfdirectory)
+    plt.savefig(pgfdirectory)
     # <--- save plot 
     
     
@@ -126,7 +159,8 @@ file = open("C:/Users/kahl/Documents/Data/data_pythonplottest.txt",'r')
 data = json.loads( file.read() )
 file.close()
 # apply function
-for i in range(0,len(data[1])):
-    data[1][i] = data[1][i]* 1911.
+#for i in range(0,len(data[1])):
+#    data[1][i] = data[1][i] 
+#    data[2][i] = data[2][i] 
 
-temp = plot2d(data, ['time', 'y_1','3'] ,'test' )
+temp = plot2d(data, ['time', 'y_1','y_2'] ,'test' )
